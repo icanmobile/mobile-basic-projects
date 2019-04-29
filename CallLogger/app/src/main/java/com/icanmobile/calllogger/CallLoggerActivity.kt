de6b8bff7ab7c1179b1a0932645944eada833829
Jong Ho Baek <icanmobile@tecace.com>
2019-04-29 14:48:39 -0700

    Init CallLogger application.

---
+++ CallLogger/app/src/main/java/com/icanmobile/calllogger/CallLoggerActivity.kt
@@ -0,0 +1,12 @@
+package com.icanmobile.calllogger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CallLoggerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
